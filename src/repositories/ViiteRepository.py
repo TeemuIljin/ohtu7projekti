4d3acddb519907d515d476d1a31e0a35c821fa78
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2025-11-29 14:31:28 +0200

    Viitteen tunniste sattumanvaraisesti tai kirjoittajan ja vuoden perusteella
    - Jos kirjoittajaa tai vuotta ei anneta, luodaan id sattumanvaraisesti
    - Jos on jo viite, jolla on sama kirjoittaja ja vuosi, luodaan id sattumanvaraisesti
    - Muutoin kirjoittajan ja vuoden perusteella

--- src/repositories/ViiteRepository.py
+++ src/repositories/ViiteRepository.py
@@ -1,3 +1,5 @@
+import random
+import string
 from entities.Viite import Viite
 
 
@@ -12,13 +14,13 @@ class ViiteRepository:
 
     def anna(self):
         return self._viitteet
-    
+
     def etsi(self, muokattava):
         for viite in self._viitteet:
             if viite.tagit.get('title') == muokattava:
                 return viite
         return "Viitettä ei löytynyt."
-    
+
     def poista(self, title):
         for viite in self._viitteet:
             if viite.tagit.get('title') == title:
@@ -34,7 +36,7 @@ class ViiteRepository:
             title = viite.tagit.get('title', '').lower()
             if hakusana in title:
                 tulokset.append(viite)
-        
+
         return tulokset
 
     def tallenna(self, viite):
@@ -45,3 +47,15 @@ class ViiteRepository:
 
         self._viitteet.append(viite)
         return viite
+
+    def anna_vapaa_viite_id(self, kirjoittaja, vuosi):
+        def _anna_sattumanvarainen_id():
+            return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+        if kirjoittaja and vuosi:
+            for viite in self._viitteet:
+                if viite.anna_kirjoittaja() == kirjoittaja and viite.anna_vuosi() == vuosi:
+                    return _anna_sattumanvarainen_id()
+            return f"{kirjoittaja.replace(' ', '')}{vuosi}"
+
+        return _anna_sattumanvarainen_id()
